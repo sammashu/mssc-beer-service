7dcf122a8c6e96fd262be3de27df151bdd28c178
sammashu <sammashu@hotmail.com>
2021-01-13 15:53:05 -0500

    Fixing test, closes #10

--- src/test/java/com/alexson/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/alexson/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,6 +1,7 @@
 package com.alexson.springframework.msscbeerservice.web.controller;
 
 import com.alexson.springframework.msscbeerservice.web.model.BeerDto;
+import com.alexson.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -33,7 +35,7 @@ class BeerControllerTest {
     @Test
     void saveNewBeer() throws Exception {
 
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson =  objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(post("/api/v1/beer/")
@@ -45,12 +47,21 @@ class BeerControllerTest {
     @Test
     void updateBeerById() throws Exception{
 
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
         .contentType(MediaType.APPLICATION_JSON)
         .content(beerDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+    BeerDto getValidBeerDto(){
+        return BeerDto.builder()
+                .beerName("My Beer")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal("2.99"))
+                .upc(1231232133423L)
+                .build();
     }
 }
