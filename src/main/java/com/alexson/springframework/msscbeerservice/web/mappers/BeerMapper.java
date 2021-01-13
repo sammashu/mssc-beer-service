d78971b573235dd6ebcff1f56f7f99f7141984f9
sammashu <sammashu@hotmail.com>
2021-01-13 15:49:23 -0500

    Added mapstruct, closes #9

---
+++ src/main/java/com/alexson/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package com.alexson.springframework.msscbeerservice.web.mappers;
+
+import com.alexson.springframework.msscbeerservice.domain.Beer;
+import com.alexson.springframework.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = { DateMapper.class })
+public interface BeerMapper {
+
+    BeerDto BeerToBeerDto(Beer beer);
+
+    Beer BeerDtoToBeer(BeerDto beerDto);
+}
